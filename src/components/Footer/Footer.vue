11fe59a1080820d33afd9eb3162788f88ed93400
Yuda <fajaryudaapriliano@gmail.com>
2026-02-07 09:41:38 +0700

    07/02/2026

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import styles from "./Footer.module.css";
+</script>
+
+<template>
+  <div :class="styles.container">
+    <p :class="styles.text">mustakuusi — lahir dari pikiran yang tak diam.</p>
+  </div>
+</template>
