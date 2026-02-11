ad2d11b93fb869a56bb05fae67f334779f4e2a93
Yuda <fajaryudaapriliano@gmail.com>
2026-02-11 21:42:01 +0700

    11/02/2026

---
+++ src/components/Characters/Characters.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import CharacterCard from "./CharacterCard.vue";
+import styles from "./Characters.module.css";
+
+interface CharacterData {
+  id: string;
+  name: string;
+  imageSrc: string;
+  description: string;
+}
+
+interface CharactersProps {
+  characterSection: string;
+  characters: CharacterData[];
+}
+
+const props = defineProps<CharactersProps>();
+</script>
+
+<template>
+  <section :class="styles.container" id="characters">
+    <h2 :class="styles.title">{{ props.characterSection }}</h2>
+    <div :class="styles.characters">
+      <CharacterCard v-for="character in characters" :key="character.id" :character="character"
+                     :class="character.id === 'lato' ? styles.centered : ''"/>
+    </div>
+  </section>
+</template>
---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+export const getImageUrl = (path: string) => {
+    return `https://mustakuusi-content.vercel.app/assets/${path}`;
+}
